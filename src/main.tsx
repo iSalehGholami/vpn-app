19414fc3952ce13296776191559a055ab3209372
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2025-01-15 20:32:43 +0330

    Adding base

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { WebAppProvider } from "@vkruglikov/react-telegram-web-app";
-import { ConfigProvider } from "antd";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   // server: { https: true }, // Not needed for Vite 5+
   plugins: [mkcert(), react()],
+  base: "/vpn-app/",
 });
